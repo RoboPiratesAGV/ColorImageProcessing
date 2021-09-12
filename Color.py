4993cdd1c77bae6089912e6c9624f3e6134c6e89
Anjana R <44143225+AnjanaRajanM@users.noreply.github.com>
2021-09-12 20:12:24 +0530

    detected

--- Color.py
+++ Color.py
@@ -1,3 +1,4 @@
+import cap
 import numpy as np 
 import cv2 
 
@@ -10,13 +11,13 @@ while(1):
 
   
 
-    # Black
+    # Red
 
-    black_lower = np.array([0, 0, 0], np.uint8) 
+    red_lower = np.array([136, 87, 111], np.uint8) 
 
-    black_upper = np.array([105, 105, 105], np.uint8) 
+    red_upper = np.array([180, 255, 255], np.uint8) 
 
-    black_mask = cv2.inRange(hsvFrame, black_lower, black_upper) 
+    red_mask = cv2.inRange(hsvFrame, red_lower, red_upper) 
 
     # Brown
 
@@ -34,15 +35,25 @@ while(1):
 
     blue_mask = cv2.inRange(hsvFrame, blue_lower, blue_upper) 
 
+    # Green
+
+    green_lower = np.array([25, 52, 72], np.uint8) 
+
+    green_upper = np.array([102, 255, 255], np.uint8) 
+
+    green_mask = cv2.inRange(hsvFrame, green_lower, green_upper) 
+
+    # Blue 
+
+    yellow_lower = np.array([22,60,200], np.uint8) 
+
+    yellow_upper = np.array([60,255,255], np.uint8) 
+
+    yellow_mask = cv2.inRange(hsvFrame, yellow_lower, yellow_upper) 
+
+
       
 
-    # Morphological Transform, Dilation 
-
-    # for each color and bitwise_and operator 
-
-    # between imageFrame and mask determines 
-
-    # to detect only that particular color 
 
     kernal = np.ones((5, 5), "uint8") 
 
@@ -50,15 +61,15 @@ while(1):
 
     # For red color 
 
-    black_mask = cv2.dilate(black_mask, kernal) 
+    red_mask = cv2.dilate(red_mask, kernal) 
 
-    res_black = cv2.bitwise_and(imageFrame, imageFrame,  
+    res_red = cv2.bitwise_and(imageFrame, imageFrame,  
 
-                              mask = black_mask) 
+                              mask = red_mask) 
 
       
 
-    # For green color 
+    # For brown color 
 
     brown_mask = cv2.dilate(brown_mask, kernal) 
 
@@ -66,7 +77,14 @@ while(1):
 
                                 mask = brown_mask) 
 
-      
+
+    # For green color 
+    green_mask = cv2.dilate(green_mask, kernal) 
+
+    res_green = cv2.bitwise_and(imageFrame, imageFrame, 
+
+                                mask = green_mask) 
+
 
     # For blue color 
 
@@ -76,105 +94,99 @@ while(1):
 
                                mask = blue_mask) 
 
+    # For yellow color 
+
+    yellow_mask = cv2.dilate(yellow_mask, kernal) 
+
+    res_ = cv2.bitwise_and(imageFrame, imageFrame, 
+
+                               mask = yellow_mask)                            
+
    
 
     # Creating contour to track red color 
 
-    contours, hierarchy = cv2.findContours(black_mask, 
-
-                                           cv2.RETR_TREE, 
-
-                                           cv2.CHAIN_APPROX_SIMPLE) 
-
+    contours, hierarchy = cv2.findContours(red_mask,
+                                           cv2.RETR_TREE,
+                                           cv2.CHAIN_APPROX_SIMPLE)
       
-
-    for pic, contour in enumerate(contours): 
-
-        area = cv2.contourArea(contour) 
-
-        if(area > 300): 
-
-            x, y, w, h = cv2.boundingRect(contour) 
-
-            imageFrame = cv2.rectangle(imageFrame, (x, y),  
-
-                                       (x + w, y + h),  
-
-                                       (0, 0, 0), 2) 
-
+    for pic, contour in enumerate(contours):
+        area = cv2.contourArea(contour)
+        if(area > 300):
+            x, y, w, h = cv2.boundingRect(contour)
+            imageFrame = cv2.rectangle(imageFrame, (x, y), 
+                                       (x + w, y + h), 
+                                       (0, 0, 255), 2)
               
+            cv2.putText(imageFrame, "Red Colour", (x, y),
+                        cv2.FONT_HERSHEY_SIMPLEX, 1.0,
+                        (0, 0, 255))    
 
-            cv2.putText(imageFrame, "Black Colour", (x, y), 
-
-                        cv2.FONT_HERSHEY_SIMPLEX, 1.0, 
-
-                        (0, 0, 0))     
-
-  
-
+                        
     # Creating contour to track green color 
 
-    contours, hierarchy = cv2.findContours(brown_mask, 
-
-                                           cv2.RETR_TREE, 
-
-                                           cv2.CHAIN_APPROX_SIMPLE) 
-
+    contours, hierarchy = cv2.findContours(green_mask,
+                                           cv2.RETR_TREE,
+                                           cv2.CHAIN_APPROX_SIMPLE)
       
-
-    for pic, contour in enumerate(contours): 
-
-        area = cv2.contourArea(contour) 
-
-        if(area > 300): 
-
-            x, y, w, h = cv2.boundingRect(contour) 
-
-            imageFrame = cv2.rectangle(imageFrame, (x, y),  
-
-                                       (x + w, y + h), 
-
-                                       (42, 42, 165), 2) 
-
+    for pic, contour in enumerate(contours):
+        area = cv2.contourArea(contour)
+        if(area > 300):
+            x, y, w, h = cv2.boundingRect(contour)
+            imageFrame = cv2.rectangle(imageFrame, (x, y), 
+                                       (x + w, y + h),
+                                       (0, 255, 0), 2)
               
+            cv2.putText(imageFrame, "Green Colour", (x, y),
+                        cv2.FONT_HERSHEY_SIMPLEX, 
+                        1.0, (0, 255, 0))
 
-            cv2.putText(imageFrame, "Brown Colour", (x, y), 
-
-                        cv2.FONT_HERSHEY_SIMPLEX,  
-
-                        1.0, (42, 42, 165)) 
-
-  
 
     # Creating contour to track blue color 
 
-    contours, hierarchy = cv2.findContours(blue_mask, 
-
-                                           cv2.RETR_TREE, 
-
-                                           cv2.CHAIN_APPROX_SIMPLE) 
-
-    for pic, contour in enumerate(contours): 
-
-        area = cv2.contourArea(contour) 
-
-        if(area > 300): 
-
-            x, y, w, h = cv2.boundingRect(contour) 
-
-            imageFrame = cv2.rectangle(imageFrame, (x, y), 
-
-                                       (x + w, y + h), 
-
-                                       (255, 0, 0), 2) 
-
+    contours, hierarchy = cv2.findContours(blue_mask,
+                                           cv2.RETR_TREE,
+                                           cv2.CHAIN_APPROX_SIMPLE)
+    for pic, contour in enumerate(contours):
+        area = cv2.contourArea(contour)
+        if(area > 300):
+            x, y, w, h = cv2.boundingRect(contour)
+            imageFrame = cv2.rectangle(imageFrame, (x, y),
+                                       (x + w, y + h),
+                                       (255, 0, 0), 2)
               
+            cv2.putText(imageFrame, "Blue Colour", (x, y),
+                        cv2.FONT_HERSHEY_SIMPLEX,
+                        1.0, (255, 0, 0))
 
-            cv2.putText(imageFrame, "Blue Colour", (x, y), 
+     # Creating contour to track yellow color 
+    contours, hierarchy = cv2.findContours(blue_mask,
+                                           cv2.RETR_TREE,
+                                           cv2.CHAIN_APPROX_SIMPLE)
+    for pic, contour in enumerate(contours):
+        area = cv2.contourArea(contour)
+        if(area > 300):
+            x, y, w, h = cv2.boundingRect(contour)
+            imageFrame = cv2.rectangle(imageFrame, (x, y), (x + w, y + h), (255,255,0), 2)
+            cv2.putText(imageFrame, "Yellow Colour", (x, y),
+                        cv2.FONT_HERSHEY_SIMPLEX,
+                        1.0,  (255,255,0))
 
-                        cv2.FONT_HERSHEY_SIMPLEX, 
-
-                        1.0, (255, 0, 0)) 
+    #Creating contour to track brown color
+    contours, hierarchy = cv2.findContours(blue_mask,
+                                           cv2.RETR_TREE,
+                                           cv2.CHAIN_APPROX_SIMPLE)
+    for pic, contour in enumerate(contours):
+        area = cv2.contourArea(contour)
+        if(area > 300):
+            x, y, w, h = cv2.boundingRect(contour)
+            imageFrame = cv2.rectangle(imageFrame, (x, y),
+                                       (x + w, y + h),
+                                       (0, 75, 150), 2)
+              
+            cv2.putText(imageFrame, "Brown Colour", (x, y),
+                        cv2.FONT_HERSHEY_SIMPLEX,
+                        0.7, (0, 75, 150))
 
               
 
